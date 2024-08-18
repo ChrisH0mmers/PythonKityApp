09bef714267ed7591aa1182f2fafbc0123517eb4
ChrisH0mmers <chrishamers@outlook.com>
2024-08-18 15:38:40 +0200

    Installed Kivy

--- Main/Application.py
+++ Main/Application.py
@@ -1,2 +1,9 @@
-import os
-os.system('cls')
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text='Hello, Kivy!')
+
+if __name__ == '__main__':
+    MyApp().run()
