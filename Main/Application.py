42d4a5c3462a0f3b48730c7f79cdab59176b7466
ChrisH0mmers <chrishamers@outlook.com>
2024-08-18 16:05:45 +0200

    Added some things

--- Main/Application.py
+++ Main/Application.py
@@ -1,8 +1,18 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+
+class MyBoxLayout(BoxLayout):
+    def OnSubmit(self):
+        TextInput = self.ids.TextInput
+        InputText = TextInput.text
+        OutputLabel = self.ids.OutputLabel
+        OutputLabel.text = InputText
+        TextInput.text = ""
 
 class Shopping(App):
-    pass
+    def build(self):
+        return MyBoxLayout()
 
 
 
