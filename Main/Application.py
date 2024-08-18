2a4ffab71d4b168f840120ea9eecc4b4f7ef30d7
ChrisH0mmers <chrishamers@outlook.com>
2024-08-18 15:59:26 +0200

    Started

--- Main/Application.py
+++ Main/Application.py
@@ -1,9 +1,11 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
-class MyApp(App):
-    def build(self):
-        return Label(text='Hello, Kivy!')
+class Shopping(App):
+    pass
+
+
 
 if __name__ == '__main__':
-    MyApp().run()
+    Shopping().run()
+
