6b0b3f7942edd485a629174bface43a3bd47bb4f
ChrisH0mmers <chrishamers@outlook.com>
2024-08-18 16:11:20 +0200

    Added some things

--- Main/Application.py
+++ Main/Application.py
@@ -1,7 +1,6 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
-
 class MyBoxLayout(BoxLayout):
     def OnSubmit(self):
         TextInput = self.ids.TextInput
@@ -14,8 +13,5 @@ class Shopping(App):
     def build(self):
         return MyBoxLayout()
 
-
-
 if __name__ == '__main__':
     Shopping().run()
-
